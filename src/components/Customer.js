cffb502373ccf15b13f3a7d20355058a45b786ac
daryn-brown <darynnbrown@gmail.com>
2023-10-17 22:27:40 -0500

    changes to background

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,40 +1,41 @@
-import React, {useState} from 'react';
-// import { useLocation, Link, useNavigate } from "react-router-dom";
-import { useLocation, Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-// import { Container, makeStyles, Paper, Button } from "@mui/material";
-import { Container, Paper, Button } from '@mui/material';
+import React, { useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Container, Paper, Button } from "@mui/material";
 
 export default function Customer(props) {
-    const location = useLocation();
-    // const navigate = useNavigate();
-    const paperStyle={padding: '50px 20px', width:600, margin:"20px auto"}
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastname] = useState('')
-    const [contactNumber, setContactNumber] = useState('')
-    const [submitted, setSubmitted] = useState(false)
+  const location = useLocation();
+  const paperStyle = {
+    padding: "3.125rem 1.25rem", // 50px 20px
+    width: "37.5rem", // 600px
+    margin: "1.25rem auto", // 20px auto
+  };
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
-    const data = location.state?.data;
-    const registeredCourses = data;
-    const customer = {firstName,lastName,contactNumber,registeredCourses};
+  const data = location.state?.data;
+  const registeredCourses = data;
+  const customer = { firstName, lastName, contactNumber, registeredCourses };
 
-    console.log(data)
-    const register = (e)=>{
-        e.preventDefault()
-        console.log(customer)
-        fetch("https://registration-backend-flw6.onrender.com/customer/add", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(customer),
-        }).then(() => {
-          console.log("New Customer registered");
-          console.log(submitted);
-          setSubmitted(true);
-          console.log(submitted);
-        });
-    }
-    
+  console.log(data);
+  const register = (e) => {
+    e.preventDefault();
+    console.log(customer);
+    fetch("https://registration-backend-flw6.onrender.com/customer/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(customer),
+    }).then(() => {
+      console.log("New Customer registered");
+      console.log(submitted);
+      setSubmitted(true);
+      console.log(submitted);
+    });
+  };
+
   return (
     <div
       style={{
@@ -42,6 +43,7 @@ export default function Customer(props) {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
+        background: "linear-gradient(to bottom, white, skyblue)",
       }}
     >
       <Box
@@ -52,8 +54,18 @@ export default function Customer(props) {
         noValidate
         autoComplete="off"
       >
+        <img
+          src="/logo.png"
+          alt="logo"
+          style={{
+            position: "absolute",
+            top: "10rem", // 160px
+            width: "9.375rem", // 150px
+            zIndex: 1,
+          }}
+        />
         <Paper elevation={3} style={paperStyle}>
-          <h1>Register Now!</h1>
+          <h1 style={{ textAlign: "center" }}>Register Now!</h1>
           <TextField
             required
             type={"text"}
@@ -63,7 +75,7 @@ export default function Customer(props) {
             fullWidth
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: "1rem" }} // 16px
           />
           <TextField
             required
@@ -74,7 +86,7 @@ export default function Customer(props) {
             fullWidth
             value={lastName}
             onChange={(e) => setLastname(e.target.value)}
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: "1rem" }} // 16px
           />
           <TextField
             required
@@ -85,7 +97,7 @@ export default function Customer(props) {
             fullWidth
             value={contactNumber}
             onChange={(e) => setContactNumber(e.target.value)}
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: "1rem" }} // 16px
           />
           <TextField
             id="outlined-read-only-input"
@@ -93,12 +105,19 @@ export default function Customer(props) {
             defaultValue={data}
             fullWidth
             InputProps={{ readOnly: true }}
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: "1rem" }} // 16px
           />
-
-          <Button variant="contained" onClick={register}>
-            Submit
-          </Button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "1rem", // 16px
+            }}
+          >
+            <Button variant="contained" onClick={register}>
+              Submit
+            </Button>
+          </div>
           {submitted && (
             <Link
               to="/profile"
